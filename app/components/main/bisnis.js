ba1731946ec977d8bdef496d9d8291319f6d01a1
syafar <target44135@gmail.com>
2024-10-05 14:33:01 +0800

    update bisnis responsif

--- app/components/main/bisnis.js
+++ app/components/main/bisnis.js
@@ -1,12 +1,17 @@
 const Bisnis = () => {
   return (
-    <div className="bg-accent p-10">
+    <div className="bg-primary p-5 md:p-10">
       <h1 className="text-2xl font-bold text-center">Bisnis Kami</h1>
-      <div className="flex justify-between pt-5 mx-auto">
-        <img className="w-[60%] h-[20rem]" src="bisnis/gerai.jpg" />
-        <div className="p-10 flex flex-col justify-center gap-5">
+      <div className="flex flex-col md:flex-row justify-between pt-5 mx-auto">
+        <div
+          className="bg-cover h-[15rem] md:h-[20rem] md:w-[60%]"
+          style={{
+            backgroundImage: "url(bisnis/gerai.jpg)",
+          }}
+        ></div>
+        <div className="p-10 flex flex-col justify-center gap-5 md:w-[40%]">
           <img src="logo/gerai.jpg" />
-          <ul className="list-disc font-bold pl-[25%]">
+          <ul className="list-disc font-bold mx-auto">
             <li>Gerai utama juanda</li>
             <li>Bandara samarinda</li>
             <li>Bandara balikpapan</li>
@@ -16,15 +21,23 @@ const Bisnis = () => {
           </button>
         </div>
       </div>
-      <div className="flex justify-between pt-5 mx-auto">
-        <div className="p-10 flex flex-col justify-center gap-5">
-          <img src="logo/jajan.png" />
+      <div className="flex flex-col-reverse md:flex-row justify-between pt-5 mx-auto">
+        <div className="p-10 flex flex-col gap-5 md:w-[40%]">
+          <img
+            className="w-[10rem] mx-auto md:w-[60%] md:ml-[20%]"
+            src="logo/jajan.png"
+          />
           <p className="text-center font-bold">9 Cabang di kota Samarinda</p>
-          <button className="px-full py-2 text-black bg-yellow-300">
+          <button className="px-full py-2 text-black bg-black text-white">
             <a href="#">Jajan Panglima</a>
           </button>
         </div>
-        <img className="h-[20rem]" src="bisnis/jajan.jpg" />
+        <div
+          className="bg-cover h-[15rem] md:h-[20rem] md:w-[60%]"
+          style={{
+            backgroundImage: "url(bisnis/jajan.jpg)",
+          }}
+        ></div>
       </div>
     </div>
   );

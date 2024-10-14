b7f3906e568fd4b13f7cf32b1040904e099a84d9
syafar <target44135@gmail.com>
2024-10-14 14:46:32 +0800

    update logo leyout.j

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+const logo = "../public/logo/prg.png";
 
 export const metadata = {
   title: "Panglima Roqiiqu Group",
@@ -8,6 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
+      <head>
+        <link
+          rel="icon"
+          href="https://raw.githubusercontent.com/Syafar44/assets/refs/heads/main/assets/image/Desain%20tanpa%20judul%20(3).png"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );

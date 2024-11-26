c86877368f9915adc3557f6a9da57748741a8399
syafar <target44135@gmail.com>
2024-11-26 08:24:07 +0800

    memperbaiki penamaan di hero

--- app/components/main/hero.js
+++ app/components/main/hero.js
@@ -3,7 +3,7 @@ const Hero = () => {
     <div id="beranda" className="flex flex-col md:flex-row px-5 pt-5 md:px-0 md:pt-0 md:justify-between md:items-center md:h-72 overflow-hidden md:pl-10 bg-primary">
       <div>
         <h2 className="text-xl md:text-3xl font-bold">Website Resmi</h2>
-        <h1 className="text-2xl md:text-4xl font-bold mb-5">Panglima Roqqiu Group</h1>
+        <h1 className="text-2xl md:text-4xl font-bold mb-5">Panglima Roqiiqu Group</h1>
         <p className="md:w-[80%] text-xs md:text-sm mb-3">
           Kami berkomitmen menjadi perusahaan syariah kelas dunia yang dicintai,
           menyediakan produk halal berkualitas, serta berkontribusi bagi
